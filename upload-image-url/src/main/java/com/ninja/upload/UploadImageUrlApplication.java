1c79fa644befc591a650d369a5581c8f875facc3
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-07-20 16:33:31 +0630

    project setup and running

---
+++ upload-image-url/src/main/java/com/ninja/upload/UploadImageUrlApplication.java
@@ -0,0 +1,13 @@
+package com.ninja.upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadImageUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadImageUrlApplication.class, args);
+	}
+
+}
---
+++ upload-image-url/src/test/java/com/ninja/upload/UploadImageUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ninja.upload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadImageUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
